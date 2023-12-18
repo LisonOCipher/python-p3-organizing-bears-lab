63beb3132fa2d6160d15d0faaadb21ee5572e901
Danson Lino <danson.lino@student.moringaschool.com>
2023-12-18 13:54:49 +0300

    committed

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES
+  (1, 'Mr.Chocolate', 15, 'M', 'black', 'peaceful', 1),
+  (2, 'Rowdy', 10, 'M', 'brown', 'playful', 1),
+  (3, 'Tabitha', 20, 'F', 'white', 'gentle', 1),
+  (4, 'Sergeant Brown', 25, 'M', 'brown', 'aggressive', 0),
+  (5, 'Melissa', 17, 'F', 'black', 'hugz', 1),
+  (6, 'Grinch', 20, 'M', 'brown', 'angry', 0),
+  (7, 'Wendy', 23, 'F', 'black', 'aggressive', 0),
+  (8, NULL, 15, 'M', 'white', 'peaceful', 1);
